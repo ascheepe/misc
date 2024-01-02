96e39e3787721c79811323802b01c23108a73758
Axel Scheepers <no@spam.please>
2024-01-02 19:40:51 +0100

    try 'modern' style again

--- makemono.c
+++ makemono.c
@@ -6,198 +6,202 @@
 #define MAXLINELEN 8192
 
 enum hues {
-	WHITE, AMBER, CYAN, GREEN
+    WHITE, AMBER, CYAN, GREEN
 };
 
 struct rgb {
-	unsigned char r, g, b;
+    unsigned char r, g, b;
 };
 
-static void *
-xmalloc(size_t size)
+static void *xmalloc(size_t size)
 {
-	void *ptr;
+    void *ptr;
 
-	ptr = malloc(size);
-	if (ptr == NULL) {
-		fprintf(stderr, "Out of memory!\n");
-		exit(EXIT_FAILURE);
-	}
+    ptr = malloc(size);
+    if (ptr == NULL) {
+        fprintf(stderr, "Out of memory!\n");
+        exit(EXIT_FAILURE);
+    }
 
-	return ptr;
+    return ptr;
 }
 
-struct rgb *
-parse_hexcolor(const char *str, size_t *len)
+struct rgb *parse_hexcolor(const char *str, size_t *length)
 {
-	struct rgb *color = NULL;
-	unsigned int r, g, b;
+    struct rgb *color = NULL;
+    unsigned int r, g, b;
 
-	if (*str != '#')
-		return NULL;
+    if (*str != '#') {
+        return NULL;
+    }
 
-	if (sscanf(str, "#%02x%02x%02x", &r, &g, &b) == 3) {
-		if (!(isspace(str[8]) || str[8] == '\0'))
-			return NULL;
+    if (sscanf(str, "#%02x%02x%02x", &r, &g, &b) == 3) {
+        if (!(isspace(str[8]) || str[8] == '\0')) {
+            return NULL;
+        }
 
-		color = xmalloc(sizeof(*color));
-		color->r = r;
-		color->g = g;
-		color->b = b;
-		*len = 7;
-	} else if (sscanf(str, "#%1x%1x%1x", &r, &g, &b) == 3) {
-		if (!(isspace(str[8]) || str[8] == '\0'))
-			return NULL;
+        color = xmalloc(sizeof(*color));
+        color->r = r;
+        color->g = g;
+        color->b = b;
+        *length = 7;
+    } else if (sscanf(str, "#%1x%1x%1x", &r, &g, &b) == 3) {
+        if (!(isspace(str[8]) || str[8] == '\0')) {
+            return NULL;
+        }
 
-		color = xmalloc(sizeof(*color));
-		color->r = r + 16 * r;
-		color->g = g + 16 * g;
-		color->b = b + 16 * b;
-		*len = 4;
-	}
+        color = xmalloc(sizeof(*color));
+        color->r = r + 16 * r;
+        color->g = g + 16 * g;
+        color->b = b + 16 * b;
+        *length = 4;
+    }
 
-	return color;
+    return color;
 }
 
-static void
-print_hexcolor(const struct rgb *color, FILE *out)
+static void print_hexcolor(const struct rgb *color, FILE *out)
 {
-	fprintf(out, "#%02x%02x%02x", color->r, color->g, color->b);
+    fprintf(out, "#%02x%02x%02x", color->r, color->g, color->b);
 }
 
-static struct rgb *
-parse_rgbcolor(const char *str, size_t *len)
+static struct rgb *parse_rgbcolor(const char *str, size_t *length)
 {
-	struct rgb *color = NULL;
-	int r, g, b;
+    struct rgb *color = NULL;
+    int r, g, b;
 
-	if (*str != 'r')
-		return NULL;
+    if (*str != 'r') {
+        return NULL;
+    }
 
-	if (sscanf(str, "rgb(%d, %d, %d)", &r, &g, &b) == 3) {
-		const char *p;
+    if (sscanf(str, "rgb(%d, %d, %d)", &r, &g, &b) == 3) {
+        const char *color_start = str;
+        const char *color_end = str;
 
-		if (r < 0 || r > 256 || g < 0 || g > 256 || b < 0 || b > 256) {
-			fprintf(stderr, "WARNING: invalid color ");
-			fprintf(stderr, "rgb(%d, %d, %d) found.", r, g, b);
-			return NULL;
-		}
-		color = xmalloc(sizeof(*color));
-		color->r = r;
-		color->g = g;
-		color->b = b;
+        if (r < 0 || r > 256 || g < 0 || g > 256 || b < 0 || b > 256) {
+            fprintf(stderr, "WARNING: invalid color ");
+            fprintf(stderr, "rgb(%d, %d, %d) found.", r, g, b);
+            return NULL;
+        }
+        color = xmalloc(sizeof(*color));
+        color->r = r;
+        color->g = g;
+        color->b = b;
 
-		for (p = str; *p != ')'; ++p)
-			;
-		*len = ++p - str;
-	}
+        while (*color_end != ')') {
+            ++color_end;
+        }
+        ++color_end;
+        *length = color_end - color_start;
+    }
 
-	return color;
+    return color;
 }
 
-static void
-print_rgbcolor(const struct rgb *color, FILE *out)
+static void print_rgbcolor(const struct rgb *color, FILE *out)
 {
-	fprintf(out, "rgb(%d, %d, %d)", color->r, color->g, color->b);
+    fprintf(out, "rgb(%d, %d, %d)", color->r, color->g, color->b);
 }
 
-static void
-color_to_mono(struct rgb *color, enum hues hue)
+static void color_to_mono(struct rgb *color, enum hues hue)
 {
-	int gray;
+    int gray;
 
-	gray = (int)((color->r * 299L + 500) / 1000 +
-	    (color->g * 587L + 500) / 1000 + (color->b * 114L + 500) / 1000);
+    gray = (int) ((color->r * 299L + 500) / 1000
+                + (color->g * 587L + 500) / 1000
+                + (color->b * 114L + 500) / 1000);
 
-	switch (hue) {
-	default:
-	case WHITE:
-		color->r = gray;
-		color->g = gray;
-		color->b = gray;
-		break;
-	case AMBER:
-		color->r = gray;
-		color->g = (unsigned char)(gray * 191L / 255);
-		color->b = 0;
-		break;
-	case CYAN:
-		color->r = 0;
-		color->g = gray;
-		color->b = gray;
-		break;
-	case GREEN:
-		color->r = 0;
-		color->g = gray;
-		color->b = 0;
-		break;
-	}
+    switch (hue) {
+        default:
+        case WHITE:
+            color->r = gray;
+            color->g = gray;
+            color->b = gray;
+            break;
+        case AMBER:
+            color->r = gray;
+            color->g = (unsigned char) (gray * 191L / 255);
+            color->b = 0;
+            break;
+        case CYAN:
+            color->r = 0;
+            color->g = gray;
+            color->b = gray;
+            break;
+        case GREEN:
+            color->r = 0;
+            color->g = gray;
+            color->b = 0;
+            break;
+    }
 }
 
-static void
-process_line(const char *line, FILE *out, enum hues hue)
+static void process_line(const char *line, FILE *output_file, enum hues hue)
 {
-	const char *pos = line;
+    const char *current_position = line;
 
-	while (*pos != '\0') {
-		struct rgb *color;
-		size_t len;
+    while (*current_position != '\0') {
+        struct rgb *color;
+        size_t length;
 
-		if ((color = parse_hexcolor(pos, &len)) != NULL) {
-			color_to_mono(color, hue);
-			print_hexcolor(color, out);
-			free(color);
+        if ((color = parse_hexcolor(current_position, &length)) != NULL) {
+            color_to_mono(color, hue);
+            print_hexcolor(color, output_file);
+            free(color);
 
-			pos += len;
-		} else if ((color = parse_rgbcolor(pos, &len)) != NULL) {
-			color_to_mono(color, hue);
-			print_rgbcolor(color, out);
-			free(color);
+            current_position += length;
+        } else if ((color = parse_rgbcolor(current_position, &length)) !=
+                   NULL) {
+            color_to_mono(color, hue);
+            print_rgbcolor(color, output_file);
+            free(color);
 
-			pos += len;
-		} else
-			fputc(*pos++, out);
-	}
+            current_position += length;
+        } else {
+            fputc(*current_position, output_file);
+            ++current_position;
+        }
+    }
 }
 
-static enum hues
-get_hue(int argc, char **argv)
+static enum hues get_hue(int argc, char **argv)
 {
-	enum hues hue = WHITE;
+    enum hues hue = WHITE;
 
-	if (argc < 2)
-		return hue;
+    if (argc < 2) {
+        return hue;
+    }
 
-	if (argv[1][0] == '-') {
-		switch (argv[1][1]) {
-		case 'a':
-			hue = AMBER;
-			break;
-		case 'c':
-			hue = CYAN;
-			break;
-		case 'g':
-			hue = GREEN;
-			break;
-		case 'w':
-			hue = WHITE;
-			break;
-		}
-	}
+    if (argv[1][0] == '-') {
+        switch (argv[1][1]) {
+            case 'a':
+                hue = AMBER;
+                break;
+            case 'c':
+                hue = CYAN;
+                break;
+            case 'g':
+                hue = GREEN;
+                break;
+            case 'w':
+                hue = WHITE;
+                break;
+        }
+    }
 
-	return hue;
+    return hue;
 }
 
-int
-main(int argc, char **argv)
+int main(int argc, char **argv)
 {
-	char line[MAXLINELEN];
-	enum hues hue;
+    char line[MAXLINELEN];
+    enum hues hue;
 
-	hue = get_hue(argc, argv);
+    hue = get_hue(argc, argv);
 
-	while (fgets(line, sizeof(line), stdin) != NULL)
-		process_line(line, stdout, hue);
+    while (fgets(line, sizeof(line), stdin) != NULL) {
+        process_line(line, stdout, hue);
+    }
 
-	return 0;
+    return 0;
 }
--- wordfreq.c
+++ wordfreq.c
@@ -5,331 +5,328 @@
 
 #include <assert.h>
 
-#define NTOP 10
+#define TOP_COUNT 10
 #define DEFAULT_SHIFT 10
 #define MAX_SHIFT (((int)sizeof(size_t) * 8) - 1)
 
-static void
-die(const char *msg)
+static void die(const char *message)
 {
-	fprintf(stderr, "%s\n", msg);
-	exit(1);
+    fprintf(stderr, "%s\n", message);
+    exit(1);
 }
 
-static void *
-xmalloc(size_t size)
+static void *xmalloc(size_t size)
 {
-	void *p;
+    void *ptr;
 
-	p = malloc(size);
-	if (p == NULL)
-		die("malloc: out of memory.");
+    ptr = malloc(size);
+    if (ptr == NULL) {
+        die("malloc: out of memory.");
+    }
 
-	return p;
+    return ptr;
 }
 
-static void *
-xcalloc(size_t nmemb, size_t size)
+static void *xcalloc(size_t nmemb, size_t size)
 {
-	void *p;
+    void *ptr;
 
-	p = calloc(nmemb, size);
-	if (p == NULL)
-		die("calloc: out of memory.");
+    ptr = calloc(nmemb, size);
+    if (ptr == NULL) {
+        die("calloc: out of memory.");
+    }
 
-	return p;
+    return ptr;
 }
 
-static void *
-xrealloc(void *p, size_t size)
+static void *xrealloc(void *ptr, size_t size)
 {
-	void *q;
+    void *new_ptr;
 
-	q = realloc(p, size);
-	if (q == NULL)
-		die("realloc: out of memory.");
+    new_ptr = realloc(ptr, size);
+    if (new_ptr == NULL) {
+        die("realloc: out of memory.");
+    }
 
-	return q;
+    return new_ptr;
 }
 
 #define FNV_OFFSET_BASIS ((size_t)2166136261UL)
 #define FNV_PRIME ((size_t)16777619UL)
 
-static size_t
-fnv1(const char *s)
+static size_t fnv1(const char *str)
 {
-	size_t h = FNV_OFFSET_BASIS;
+    size_t hash = FNV_OFFSET_BASIS;
 
-	while (s != NULL && *s != '\0')
-		h = (h * FNV_PRIME) ^ *s++;
+    while (str != NULL && *str != '\0') {
+        hash = (hash * FNV_PRIME) ^ *str++;
+    }
 
-	return h;
+    return hash;
 }
 
 #define hashfn(s) fnv1(s)
 
 struct hash_item {
-	const char *key;
-	size_t value;
-	struct hash_item *next;
+    const char *key;
+    size_t value;
+    struct hash_item *next;
 };
 
-static struct hash_item *
-item_new(const char *key)
+static struct hash_item *item_new(const char *key)
 {
-	struct hash_item *item;
+    struct hash_item *item;
 
-	item = xmalloc(sizeof(*item));
-	item->key = key;
-	item->value = 1;
-	item->next = NULL;
+    item = xmalloc(sizeof(*item));
+    item->key = key;
+    item->value = 1;
+    item->next = NULL;
 
-	return item;
+    return item;
 }
 
-static void
-item_free(struct hash_item *item)
+static void item_free(struct hash_item *item)
 {
-	free((void *)item->key);
-	free(item);
+    free((void *) item->key);
+    free(item);
 }
 
 struct hash_table {
-	struct hash_item **items;
-	size_t nitems;
-	size_t size;
-	int shift;
+    struct hash_item **items;
+    size_t item_count;
+    size_t size;
+    int shift;
 };
 
-static struct hash_table *
-hash_table_new(int shift)
+static struct hash_table *hash_table_new(int shift)
 {
-	struct hash_table *ht;
+    struct hash_table *hash_table;
 
-	if (shift > MAX_SHIFT)
-		die("hash_table_new: shift too large.");
+    if (shift > MAX_SHIFT) {
+        die("hash_table_new: shift too large.");
+    }
 
-	ht = xmalloc(sizeof(*ht));
-	ht->items = xcalloc(1 << shift, sizeof(*ht->items));
-	ht->shift = shift;
-	ht->size = 1 << shift;
-	ht->nitems = 0;
+    hash_table = xmalloc(sizeof(*hash_table));
+    hash_table->items = xcalloc(1 << shift, sizeof(*hash_table->items));
+    hash_table->shift = shift;
+    hash_table->size = 1 << shift;
+    hash_table->item_count = 0;
 
-	return ht;
+    return hash_table;
 }
 
-static void
-hash_table_free(struct hash_table *ht)
+static void hash_table_free(struct hash_table *hash_table)
 {
-	free(ht->items);
-	free(ht);
+    free(hash_table->items);
+    free(hash_table);
 }
 
-static void
-hash_table_add(struct hash_table *ht, struct hash_item *item)
+static void hash_table_add(struct hash_table *hash_table,
+                           struct hash_item *item)
 {
-	size_t h;
+    size_t hash;
 
-	h = hashfn(item->key) & (ht->size - 1);
-	item->next = ht->items[h];
-	ht->items[h] = item;
-	++ht->nitems;
+    hash = hashfn(item->key) & (hash_table->size - 1);
+    item->next = hash_table->items[hash];
+    hash_table->items[hash] = item;
+    ++hash_table->item_count;
 }
 
-static struct hash_item *
-hash_table_get(struct hash_table *ht, const char *key)
+static struct hash_item *hash_table_get(struct hash_table *hash_table,
+                                        const char *key)
 {
-	struct hash_item *item = NULL;
-	size_t h;
+    struct hash_item *item = NULL;
+    size_t h;
 
-	h = hashfn(key) & (ht->size - 1);
-	item = ht->items[h];
+    h = hashfn(key) & (hash_table->size - 1);
+    item = hash_table->items[h];
 
-	while (item != NULL && strcmp(item->key, key) != 0)
-		item = item->next;
+    while (item != NULL && strcmp(item->key, key) != 0) {
+        item = item->next;
+    }
 
-	return item;
+    return item;
 }
 
-static void
-hash_table_foreach(struct hash_table *ht, void (*fn)(struct hash_item *))
+static void hash_table_foreach(struct hash_table *hash_table,
+                               void (*function)(struct hash_item *))
 {
-	size_t i;
+    size_t i;
 
-	for (i = 0; i < ht->size; ++i) {
-		struct hash_item *item = ht->items[i];
+    for (i = 0; i < hash_table->size; ++i) {
+        struct hash_item *item = hash_table->items[i];
 
-		while (item != NULL) {
-			struct hash_item *next = item->next;
+        while (item != NULL) {
+            struct hash_item *next_item = item->next;
 
-			fn(item);
-			item = next;
-		}
-	}
+            function(item);
+            item = next_item;
+        }
+    }
 }
 
-static void
-hash_table_grow(struct hash_table **htpp)
+static void hash_table_grow(struct hash_table **hash_table_ptr_ptr)
 {
-	struct hash_table *ht = *htpp;
-	struct hash_table *newht;
-	size_t i;
+    struct hash_table *hash_table = *hash_table_ptr_ptr;
+    struct hash_table *new_hash_table;
+    size_t i;
 
-	if ((ht->shift + 1) > MAX_SHIFT)
-		die("hash_table_grow: no room.");
-
+    if ((hash_table->shift + 1) > MAX_SHIFT) {
+        die("hash_table_grow: no room.");
+    }
 #if STATS
-	printf("--> grow hash_table to %d entries.\n", 1 << (ht->shift + 1));
+    printf("--> grow hash_table to %d entries.\n",
+           1 << (hash_table->shift + 1));
 #endif
 
-	newht = hash_table_new(ht->shift + 1);
-	for (i = 0; i < ht->size; ++i) {
-		struct hash_item *item = ht->items[i];
+    new_hash_table = hash_table_new(hash_table->shift + 1);
+    for (i = 0; i < hash_table->size; ++i) {
+        struct hash_item *item = hash_table->items[i];
 
-		while (item != NULL) {
-			struct hash_item *next = item->next;
+        while (item != NULL) {
+            struct hash_item *next_item = item->next;
 
-			hash_table_add(newht, item);
-			item = next;
-		}
-	}
+            hash_table_add(new_hash_table, item);
+            item = next_item;
+        }
+    }
 
-	hash_table_free(ht);
+    hash_table_free(hash_table);
 
-	*htpp = newht;
+    *hash_table_ptr_ptr = new_hash_table;
 }
 
 #if STATS
-static void
-hash_table_statistics(struct hash_table *ht)
+static void hash_table_statistics(struct hash_table *hash_table)
 {
-	printf("Hash table size is %lu kb.\n",
-	    (ht->size * sizeof(ht->items[0])) / 1024);
+    printf("Hash table size is %lu kb.\n",
+           (hash_table->size * sizeof(hash_table->items[0])) / 1024);
 }
 #endif
 
-static char *
-read_word(FILE *f)
+static char *read_word(FILE *f)
 {
-	size_t len = 0, maxlen = 0;
-	char *word = NULL;
-	int ch;
+    size_t len = 0, maxlen = 0;
+    char *word = NULL;
+    int ch;
 
-	while (((ch = fgetc(f)) != EOF) && isalpha(ch)) {
-		ch = tolower(ch);
+    while (((ch = fgetc(f)) != EOF) && isalpha(ch)) {
+        ch = tolower(ch);
 
-		if (len >= maxlen) {
-			word = xrealloc(word, maxlen + 32);
-			maxlen += 32;
-		}
+        if (len >= maxlen) {
+            word = xrealloc(word, maxlen + 32);
+            maxlen += 32;
+        }
 
-		word[len++] = ch;
-	}
+        word[len++] = ch;
+    }
 
-	if (word == NULL && ch == EOF)
-		return NULL;
+    if (word == NULL && ch == EOF) {
+        return NULL;
+    }
 
-	word = xrealloc(word, len + 1);
-	word[len] = '\0';
+    word = xrealloc(word, len + 1);
+    word[len] = '\0';
 
-	return word;
+    return word;
 }
 
-static void
-add_word(struct hash_table **htpp, const char *word)
+static void add_word(struct hash_table **hash_table_ptr_ptr, const char *word)
 {
-	struct hash_table *ht = *htpp;
-	struct hash_item *item;
+    struct hash_table *hash_table = *hash_table_ptr_ptr;
+    struct hash_item *item;
 
-	item = item_new(word);
+    item = item_new(word);
 
-	/* Grow table if the load is too high. */
-	if (ht->nitems > (ht->size / 4 * 3))
-		hash_table_grow(&ht);
+    /* Grow table if the load is too high. */
+    if (hash_table->item_count > (hash_table->size / 4 * 3)) {
+        hash_table_grow(&hash_table);
+    }
 
-	hash_table_add(ht, item);
+    hash_table_add(hash_table, item);
 
-	/* Propagate any changes back to the caller. */
-	*htpp = ht;
+    /* Propagate any changes back to the caller. */
+    *hash_table_ptr_ptr = hash_table;
 }
 
-static int
-by_count_descending(const void *item_a, const void *item_b)
+static int by_count_descending(const void *item_a, const void *item_b)
 {
-	const struct hash_item *a = *((const struct hash_item **)item_a);
-	const struct hash_item *b = *((const struct hash_item **)item_b);
+    const struct hash_item *a = *((const struct hash_item **) item_a);
+    const struct hash_item *b = *((const struct hash_item **) item_b);
 
-	if (a->value < b->value)
-		return 1;
-	else if (b->value < a->value)
-		return -1;
+    if (a->value < b->value) {
+        return 1;
+    } else if (b->value < a->value) {
+        return -1;
+    }
 
-	return 0;
+    return 0;
 }
 
-int
-main(void)
+int main(void)
 {
-	struct hash_table *wordcounts = NULL;
-	struct hash_item **sorted = NULL;
-	char *word = NULL;
-	size_t nitems;
-	size_t ntop;
-	size_t i;
+    struct hash_table *wordcounts = NULL;
+    struct hash_item **sorted = NULL;
+    char *word = NULL;
+    size_t item_count;
+    size_t top_count;
+    size_t i;
 
-	wordcounts = hash_table_new(DEFAULT_SHIFT);
+    wordcounts = hash_table_new(DEFAULT_SHIFT);
 
-	while ((word = read_word(stdin)) != NULL) {
-		struct hash_item *item = NULL;
+    while ((word = read_word(stdin)) != NULL) {
+        struct hash_item *item = NULL;
 
-		if (word[0] == '\0') {
-			free(word);
-			continue;
-		}
+        if (word[0] == '\0') {
+            free(word);
+            continue;
+        }
 
-		item = hash_table_get(wordcounts, word);
+        item = hash_table_get(wordcounts, word);
 
-		/* If it exists increase count, otherwise add it. */
-		if (item != NULL) {
-			free(word);
-			++item->value;
-		} else
-			add_word(&wordcounts, word);
-	}
+        /* If it exists increase count, otherwise add it. */
+        if (item != NULL) {
+            free(word);
+            ++item->value;
+        } else {
+            add_word(&wordcounts, word);
+        }
+    }
 
 #if STATS
-	hash_table_statistics(wordcounts);
+    hash_table_statistics(wordcounts);
 #endif
 
-	/* Sort the hash_table into the sorted array. */
-	sorted = xcalloc(wordcounts->nitems, sizeof(*sorted));
+    /* Sort the hash_table into the sorted array. */
+    sorted = xcalloc(wordcounts->item_count, sizeof(*sorted));
 
-	for (i = nitems = 0; i < wordcounts->size; ++i) {
-		struct hash_item *item = wordcounts->items[i];
+    for (i = item_count = 0; i < wordcounts->size; ++i) {
+        struct hash_item *item = wordcounts->items[i];
 
-		while (item != NULL) {
-			sorted[nitems++] = item;
-			item = item->next;
-		}
-	}
+        while (item != NULL) {
+            sorted[item_count++] = item;
+            item = item->next;
+        }
+    }
 
-	assert(nitems == wordcounts->nitems);
-	qsort(sorted, nitems, sizeof(*sorted), by_count_descending);
+    assert(item_count == wordcounts->item_count);
+    qsort(sorted, item_count, sizeof(*sorted), by_count_descending);
 
-	/* And print the top-N items. */
-	ntop = wordcounts->nitems < NTOP ? wordcounts->nitems : NTOP;
+    /* And print the top-N items. */
+    top_count = wordcounts->item_count < TOP_COUNT ? wordcounts->item_count :
+        TOP_COUNT;
 
-	for (i = 0; i < ntop; ++i) {
-		const char *word = sorted[i]->key;
-		const size_t count = sorted[i]->value;
+    for (i = 0; i < top_count; ++i) {
+        const char *word = sorted[i]->key;
+        const size_t count = sorted[i]->value;
 
-		printf("%lu\t%s\n", (unsigned long)count, word);
-	}
+        printf("%lu\t%s\n", (unsigned long) count, word);
+    }
 
-	/* Cleanup */
-	free(sorted);
-	hash_table_foreach(wordcounts, item_free);
-	hash_table_free(wordcounts);
+    /* Cleanup */
+    free(sorted);
+    hash_table_foreach(wordcounts, item_free);
+    hash_table_free(wordcounts);
 
-	return 0;
+    return 0;
 }
