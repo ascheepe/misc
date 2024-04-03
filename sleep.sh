f9eaab4b352d58958c1f45b62f2bd1c76eaede07
Axel Scheepers <no@spam.please>
2024-04-03 20:12:44 +0200

    move most of the loop body to a function

--- sleep.sh
+++ sleep.sh
@@ -2,16 +2,30 @@
 
 _is_valid_argument()
 {
-	if ! echo $1 | grep -q '^[0-9][0-9]*[sSmMhH]\?$'; then
-		return 1
+	if echo $1 | grep -q '^[0-9][0-9]*[sSmMhH]\?$'; then
+		return 0
 	fi
 
-	return 0
+	return 1
 }
 
-_split_argument()
+_arg_to_time()
 {
-	echo $1 | sed 's/^\([0-9][0-9]*\)\([sSmMhH]\?\)$/\1 \2/'
+	set -- $(echo $1 | sed 's/^\([0-9][0-9]*\)\([sSmMhH]\?\)$/\1 \2/')
+	_num=$1
+	_unit=${2:-s}
+
+	case $_unit in
+	[sS])
+		echo $_num
+		;;
+	[mM])
+		echo $(($_num * 60))
+		;;
+	[hH])
+		echo $(($_num * 3600))
+		;;
+	esac
 }
 
 sleep()
@@ -19,25 +33,12 @@ sleep()
 	_time=0
 	for _arg; do
 		if ! _is_valid_argument "$_arg"; then
-			echo "invalid argument: $_arg" >&2
+			echo "sleep: invalid time interval '$_arg'" >&2
 			return 1
 		fi
 
-		set -- $(_split_argument "$_arg")
-		_num=${1:-0}
-		_unit=${2:-s}
+		_time=$(($_time + $(_arg_to_time $_arg)))
 
-		case $_unit in
-		[sS])
-			_time=$(($_time + $_num))
-			;;
-		[mM])
-			_time=$(($_time + $_num * 60))
-			;;
-		[hH])
-			_time=$(($_time + $_num * 3600))
-			;;
-		esac
 	done
 	command sleep $_time
 }
