395d95017f688a44216059d20290ef06dd780705
Axel Scheepers <no@spam.please>
2024-04-04 18:53:30 +0200

    on second thought do not use extra functions and make variables local (this seems supported in about any bourne shell now)

--- sleep.sh
+++ sleep.sh
@@ -1,44 +1,34 @@
 #! /bin/sh
 
-_is_valid_argument()
-{
-	if echo $1 | grep -q '^[0-9][0-9]*[sSmMhH]\?$'; then
-		return 0
-	fi
-
-	return 1
-}
-
-_arg_to_time()
-{
-	set -- $(echo $1 | sed 's/^\([0-9][0-9]*\)\([sSmMhH]\?\)$/\1 \2/')
-	_num=$1
-	_unit=${2:-s}
-
-	case $_unit in
-	[sS])
-		echo $_num
-		;;
-	[mM])
-		echo $(($_num * 60))
-		;;
-	[hH])
-		echo $(($_num * 3600))
-		;;
-	esac
-}
-
 sleep()
 {
-	_time=0
-	for _arg; do
-		if ! _is_valid_argument "$_arg"; then
-			echo "sleep: invalid time interval '$_arg'" >&2
+	local value unit
+	local time=0 arg
+
+	for arg; do
+		if ! echo $arg | grep -q '^[0-9][0-9]*[sSmMhH]\?$'; then
+			echo "sleep: invalid time unit '$arg'" >&2
 			return 1
 		fi
 
-		_time=$(($_time + $(_arg_to_time $_arg)))
+		set -- $(echo $arg | \
+		    sed 's/^\([0-9][0-9]*\)\([sSmMhH]\?\)$/\1 \2/')
 
+		value=$1
+		unit=${2:-s}
+
+		case $unit in
+		[sS])
+			time=$(($time + $value))
+			;;
+		[mM])
+			time=$(($time + $value * 60))
+			;;
+		[hH])
+			time=$(($time + $value * 3600))
+			;;
+		esac
 	done
-	command sleep $_time
+
+	command sleep $time
 }
